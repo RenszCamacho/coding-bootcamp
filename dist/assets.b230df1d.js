74fec76b368c29f963504d70d43941c1d8b3d514
Renszo Camacho <varsovia84@gmail.com>
2021-01-26 15:13:01 +0100

    Adding dist dir.

---
+++ dist/assets.b230df1d.js
@@ -0,0 +1,10 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"KIJD":[function(require,module,exports) {
+
+},{}],"vRFQ":[function(require,module,exports) {
+
+},{"./../images/pattern-curve.svg":[["pattern-curve.3a629938.svg","IQ69"],"IQ69"],"./../images/pattern-bg.svg":[["pattern-bg.3ee36582.svg","Sfml"],"Sfml"],"./../images/pattern-quotes.svg":[["pattern-quotes.e12489aa.svg","z4Hg"],"z4Hg"]}],"FVPG":[function(require,module,exports) {
+var e=document.getElementById("next"),t=document.getElementById("previous"),n=function(e){var t=document.querySelectorAll('[class$="--tanya"],[class$="--john"]');e.addEventListener("click",function(){t.forEach(function(e){return e.classList.toggle("show")})})};n(e),n(t);
+},{}],"zBiA":[function(require,module,exports) {
+"use strict";require("normalize.css"),require("./sass/main.scss"),require("./js/main.js");
+},{"normalize.css":"KIJD","./sass/main.scss":"vRFQ","./js/main.js":"FVPG"}]},{},["zBiA"], null)
+//# sourceMappingURL=/assets.b230df1d.js.map
